1d0f4302755e0dd7834dde643f8a3b448ddf21d2
Lalani <slalani@vols.utk.edu>
2018-04-21 22:50:37 -0400

    added knight functionality

--- ClassFiles/Knight.js
+++ ClassFiles/Knight.js
@@ -1,11 +1,37 @@
 class Knight {
 
   constructor(suit, x, y, darkID, lightID) {
+    this.points = 3;
     this.suit = suit;
     this.x = x;
     this.y = y;
     this.darkID = darkID;
     this.lightID = lightID;
+  }
+
+  checkMove(board, x, y) {
+    var signX = -1;
+    var signY = -1;
+    if (x > this.x) signX = 1;
+    if (y > this.y) signY = 1;
+
+    if ((this.x + (signX * 3) == x) && (this.y + (signY) == y)) {
+      var temp = board.objectAtIndex(x, y);
+      if (temp == null) {
+        return true;
+      } else {
+        if (temp.suit != this.suit) return true;
+      }
+    } else if ((this.y + (signY * 3) == y) && (this.x + (signX) == x)) {
+      var temp = board.objectAtIndex (x, y);
+      if (temp == null) {
+        return true;
+      } else {
+        if (temp.suit != this.suit) return true;
+      }
+    }
+
+    return false;
   }
 
 }
